e3501514aed28aa57c690ce3e967be069e93d63b
VDiko <lwx_19920909@sina.cn>
2019-05-17 10:41:20 +0800

    文章功能

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,7 +18,7 @@ class ContactController extends Controller
     public function sendContactInfo(ContactMeRequest $request){
         $data = $request->only('name','phone','email');
         $data['messageLines'] = explode('\n',$request->get('message'));
-        Mail::to($data['email'])->queue(new ContactMail($data));
+        Mail::to($data['email'])->queue3(new ContactMail($data));
         return back()
             ->with("success", "消息已发送，感谢您的反馈");
     }
